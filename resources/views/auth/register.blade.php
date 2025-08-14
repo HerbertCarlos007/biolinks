342d95dfb7a11cc49bbe0f2ffe7069870689c463
HerbertCarlos007 <herbert.saopaulino@hotmail.com>
2025-08-13 21:31:03 -0300

    feat: register user

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,13 +1,20 @@
 <div>
-    <h1>Login</h1>
+    <h1>Register</h1>
 
     @if($messagem = session()->get('messagem'))
         <div>{{$messagem}}</div>
     @endif
 
-    <form action="{{route('login')}}" method="post">
+    <form action="{{route('register')}}" method="post">
         @csrf
 
+        <div>
+            <input name="name" placeholder="Name"/>
+            @error('name')
+            <span>{{$message}}</span>
+            @enderror
+        </div>
+        <br/>
         <div>
             <input name="email" placeholder="E-mail" value="{{old('email')}}"/>
             @error('email')
@@ -16,12 +23,16 @@
         </div>
         <br/>
         <div>
+            <input name="email_confirmation" placeholder="E-mail Confirmation"/>
+        </div>
+        <br/>
+        <div>
             <input type="password" name="password" placeholder="Senha"/>
             @error('password')
             <span>{{$message}}</span>
             @enderror
         </div>
-
-        <button>Logar</button>
+        <br/>
+        <button>Registrar</button>
     </form>
 </div>
